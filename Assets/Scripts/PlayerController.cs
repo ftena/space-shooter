a6f07a920055e69af86066201580ff9f621a6f2f
Fran Tena <tarod.net@gmail.com>
2020-05-17 11:33:54 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public Boundary boundary;
+	public float fireRate;
+	private float nextFire;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float speed;
+	public float tilt;
+
+	void Update() {
+
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+
+			/*
+		 	* We want to instantiate a shot.
+		 	* Clones the object original and returns the clone.
+		 	*/
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+
+			GetComponent<AudioSource>().Play();
+		}
+	}
+
+	void FixedUpdate() {		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		/*
+		 * Constraint position
+		 */
+		GetComponent<Rigidbody>().position = new Vector3 
+		(
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+
+
+
+}
